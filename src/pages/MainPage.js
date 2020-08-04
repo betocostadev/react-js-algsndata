718205017cad20de73e35aae62cd4db88d6b23e1
betocostadev <robertomarx@gmail.com>
2020-08-04 08:18:56 -0300

    added matui card test

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
+import BigO from './BigO'
 import MatTest from './MatTest'
 import './MainPage.css'
 
 class MainPage extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      page: 'Home'
+    }
   }
 
   render() {
     return (
       <div className="MainPage">
-        <h2>Main Page</h2>
-        <MatTest />
+        <h2>Javascript Algorithms and Data Structures</h2>
+        {
+          this.state.page === 'Home' ?
+            <div className="menu">
+              <h3>Algorithms</h3>
+              <BigO />
+            </div>
+          :
+            <MatTest />
+        }
+
       </div>
     )
   }
